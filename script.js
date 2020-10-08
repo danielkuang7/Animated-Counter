d1f8565f1f8b370d3035d22c0f159258acecf9a2
Daniel Kuang <danielkuang7@github.com>
2020-10-08 08:18:37 -0400

    Rounded up the cout

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const counters = document.querySelectorAll('.counter');
-const speed = 200;
+const speed = 2000;
 
 counters.forEach(counter => {
     const updateCount = () => {
      const target = +counter.getAttribute('data-target');
      const count = +counter.innerText;
      const inc = target / speed;
+     
 
      if(count < target){
-        counter.innerText = count + inc;
+        counter.innerText = Math.ceil(count + inc);
         setTimeout(updateCount, 1);
      } else{
          count.innerText = target;
